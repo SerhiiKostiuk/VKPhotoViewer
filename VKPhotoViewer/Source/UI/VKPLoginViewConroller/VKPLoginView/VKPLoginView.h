29777e148a2d7fbc31b14666e2991d0f658aa56b
Serhii Kostiuk <k.seryoga@gmail.com>
2017-02-01 16:30:00 +0200

    add login view

---
+++ VKPhotoViewer/Source/UI/VKPLoginViewConroller/VKPLoginView/VKPLoginView.h
@@ -0,0 +1,13 @@
+//
+//  VKPLoginView.h
+//  VKPhotoViewer
+//
+//  Created by Сергій Костюк on 01.02.17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VKPLoginView : UIView
+
+@end
---
+++ VKPhotoViewer/Source/UI/VKPLoginViewConroller/VKPLoginView/VKPLoginView.m
@@ -0,0 +1,21 @@
+//
+//  VKPLoginView.m
+//  VKPhotoViewer
+//
+//  Created by Сергій Костюк on 01.02.17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import "VKPLoginView.h"
+
+@implementation VKPLoginView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
