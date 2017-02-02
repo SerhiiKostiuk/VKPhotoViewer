95adbaa7115700d54426e230a8ef8502937b913b
Serhii Kostiuk <k.seryoga@gmail.com>
2017-02-02 14:58:20 +0200

    add category on MBProgressHUD

---
+++ VKPhotoViewer/Source/Library/Categories/MBProgressHUD/MBProgressHUD+KSExtensions.h
@@ -0,0 +1,16 @@
+//
+//  MBProgressHUD+KSExtensions.h
+//  VKPhotoViewer
+//
+//  Created by Сергій Костюк on 01.02.17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
+@interface MBProgressHUD (KSExtensions)
+
++ (void)show;
++ (void)hide;
+
+@end
---
+++ VKPhotoViewer/Source/Library/Categories/MBProgressHUD/MBProgressHUD+KSExtensions.m
@@ -0,0 +1,35 @@
+//
+//  MBProgressHUD+KSExtensions.m
+//  VKPhotoViewer
+//
+//  Created by Сергій Костюк on 01.02.17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import "MBProgressHUD+KSExtensions.h"
+
+#import "AppDelegate.h"
+
+@implementation MBProgressHUD (KSExtensions)
+
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (void)show {
+    UIWindow *window = [[UIApplication sharedApplication].delegate window];
+    
+    MBProgressHUD *HUD = [self showHUDAddedTo:window animated:YES];
+    HUD.contentColor = [UIColor whiteColor];
+    HUD.bezelView.color = [UIColor blackColor];
+    HUD.backgroundView.color = [UIColor colorWithWhite:0.1 alpha:0.3];
+    HUD.minShowTime = 0.5;
+}
+
++ (void)hide {
+    UIWindow *window = [[UIApplication sharedApplication].delegate window];
+    
+    [self hideHUDForView:window animated:YES];
+}
+
+@end
