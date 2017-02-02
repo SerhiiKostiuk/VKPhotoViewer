f097d338d6f8ffdba40d45ee6a08a9ec4f290433
Serhii Kostiuk <k.seryoga@gmail.com>
2017-02-02 14:56:15 +0200

    add macros header

---
+++ VKPhotoViewer/Source/Library/Macros/KSMacro.h
@@ -0,0 +1,14 @@
+//  VKPhotoViewer
+//
+//  Created by Сергій Костюк on 01.02.17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#ifndef KSMacro_h
+#define KSMacro_h
+
+#define KSConstString(name, string) static NSString * const name   = string
+
+#define KSConstNSInteger(name, value) static const NSInteger  name = value;
+
+#endif /* KSMacro_h */
